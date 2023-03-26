d110c71953f251f0d133105c3e98a0137ce4df10
unknown <rohitkushwahac@gmail.com>
2023-03-26 23:32:45 +0530

    project Done-2

--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -29,13 +29,16 @@ export default function ContactCard() {
             id="email"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
             placeholder="name@flowbite.com"
+            name="email"
             required
+
           />
         </div>
         <div>
           <label
-            for="subject"
+            htmlFor="subject"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+
           >
             Subject
           </label>
@@ -44,12 +47,13 @@ export default function ContactCard() {
             id="subject"
             className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 "
             placeholder="Let us know how we can help you"
+            name="subject"
             required
           />
         </div>
         <div className="sm:col-span-2">
           <label
-            for="message"
+            htmlFor="message"
             className="block mb-2 text-sm font-medium text-gray-900 "
           >
             Your message
@@ -59,6 +63,8 @@ export default function ContactCard() {
             rows="6"
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
             placeholder="Leave a comment..."
+            name="message"
+            required
           ></textarea>
         </div>
         <button
